7bdf2260d401becec1e21e7785a600ac45b76cf1
s322596 <S322596@students.cdu.edu.au>
2024-08-28 22:09:05 +0930

    Front and back facing cam

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-import { bootstrapCameraKit } from '@snap/camera-kit';
+import {
+  bootstrapCameraKit,
+  CameraKitSession,
+  createMediaStreamSource,
+  Transform2D,
+} from '@snap/camera-kit';
+
+const liveRenderTarget = document.getElementById('canvas');
+const flipCamera = document.getElementById('flip');
+
+let isBackFacing = true;
+let mediaStream;
 
 (async function () {
   const cameraKit = await bootstrapCameraKit({
     apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI0NzM4NzEzLCJzdWIiOiI2NDYwYjg5Ni1hNzIwLTRhMjMtOGMyZi1hZTVlZDg2MTI4YTJ-U1RBR0lOR34zM2JhZjRiNC1hMTE0LTRhMTUtYmQxZi02YzE3OWI5YWI3MzIifQ.yNIU2DJUtajP3UEq8yN1_qcfYmRUOBUot32MQGDJcjw',
   });
-  const liveRenderTarget = document.getElementById('canvas');
-  const session = await cameraKit.createSession({ liveRenderTarget });
-  const mediaStream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-  });
 
-  await session.setSource(mediaStream);
-  await session.play();
+  const session = await cameraKit.createSession({ liveRenderTarget });
+
+  bindFlipCamera(session);  // Bind the camera flip functionality to the session
 
   const lens = await cameraKit.lensRepository.loadLens(
     'a91e3dad-8929-4ed4-967f-6b28019aa088',
@@ -20,3 +27,45 @@ import { bootstrapCameraKit } from '@snap/camera-kit';
 
   await session.applyLens(lens);
 })();
+
+function bindFlipCamera(session) {
+  flipCamera.style.cursor = 'pointer';
+
+  flipCamera.addEventListener('click', () => {
+    updateCamera(session);
+  });
+
+  updateCamera(session);
+}
+
+async function updateCamera(session) {
+  isBackFacing = !isBackFacing;
+
+  flipCamera.innerText = isBackFacing
+    ? 'Switch to Front Camera'
+    : 'Switch to Back Camera';
+
+  if (mediaStream) {
+    session.pause();
+    mediaStream.getVideoTracks()[0].stop();
+  }
+
+  mediaStream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: isBackFacing ? 'environment' : 'user',
+    },
+  });
+
+  const source = createMediaStreamSource(mediaStream, {
+    // NOTE: This is important for world facing experiences
+    cameraType: isBackFacing ? 'back' : 'front',
+  });
+
+  await session.setSource(source);
+
+  if (!isBackFacing) {
+    source.setTransform(Transform2D.MirrorX);  // Mirror the front camera
+  }
+
+  session.play();
+}
