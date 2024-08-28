df7e8d61dbaa18ed1ba9ead3b1a17129430a7c9e
s322596 <S322596@students.cdu.edu.au>
2024-08-28 23:51:26 +0930

    Cap and Elephant

--- main.js
+++ main.js
@@ -7,25 +7,33 @@ import {
 
 const liveRenderTarget = document.getElementById('canvas');
 const flipCamera = document.getElementById('flip');
+const cameraSelect = document.getElementById('cameras');
+const lensSelect = document.getElementById('lenses');
 
 let isBackFacing = true;
 let mediaStream;
+let session;
 
 (async function () {
   const cameraKit = await bootstrapCameraKit({
     apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI0NzM4NzEzLCJzdWIiOiI2NDYwYjg5Ni1hNzIwLTRhMjMtOGMyZi1hZTVlZDg2MTI4YTJ-U1RBR0lOR34zM2JhZjRiNC1hMTE0LTRhMTUtYmQxZi02YzE3OWI5YWI3MzIifQ.yNIU2DJUtajP3UEq8yN1_qcfYmRUOBUot32MQGDJcjw',
   });
 
-  const session = await cameraKit.createSession({ liveRenderTarget });
+  session = await cameraKit.createSession({ liveRenderTarget });
 
-  bindFlipCamera(session);  // Bind the camera flip functionality to the session
+  const { lenses } = await cameraKit.lensRepository.loadLensGroups(['a29e3bd4-6725-431f-b2c9-fa1a64e59abc']);
+  
+  // Populate the lenses dropdown
+  attachLensesToSelect(lenses, session);
 
-  const lens = await cameraKit.lensRepository.loadLens(
-    'a91e3dad-8929-4ed4-967f-6b28019aa088',
-    'a29e3bd4-6725-431f-b2c9-fa1a64e59abc'
-  );
+  // Set the initial lens
+  await session.applyLens(lenses[0]);
 
-  await session.applyLens(lens);
+  // Populate the camera dropdown and set the initial camera
+  attachCamerasToSelect(session);
+
+  // Bind flip camera functionality
+  bindFlipCamera(session);
 })();
 
 function bindFlipCamera(session) {
@@ -38,12 +46,10 @@ function bindFlipCamera(session) {
   updateCamera(session);
 }
 
-async function updateCamera(session) {
+async function updateCamera(session, deviceId) {
   isBackFacing = !isBackFacing;
 
-  flipCamera.innerText = isBackFacing
-    ? 'Switch to Front Camera'
-    : 'Switch to Back Camera';
+  flipCamera.innerText = isBackFacing ? 'Switch to Front Camera' : 'Switch to Back Camera';
 
   if (mediaStream) {
     session.pause();
@@ -52,12 +58,12 @@ async function updateCamera(session) {
 
   mediaStream = await navigator.mediaDevices.getUserMedia({
     video: {
+      deviceId: deviceId || undefined,
       facingMode: isBackFacing ? 'environment' : 'user',
     },
   });
 
   const source = createMediaStreamSource(mediaStream, {
-    // NOTE: This is important for world facing experiences
     cameraType: isBackFacing ? 'back' : 'front',
   });
 
@@ -69,3 +75,43 @@ async function updateCamera(session) {
 
   session.play();
 }
+
+async function attachCamerasToSelect(session) {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const cameras = devices.filter(({ kind }) => kind === 'videoinput');
+
+  cameraSelect.innerHTML = ''; // Clear previous options
+  cameras.forEach((camera, index) => {
+    const option = document.createElement('option');
+    option.value = camera.deviceId;
+    option.text = camera.label || `Camera ${index + 1}`;
+    cameraSelect.appendChild(option);
+  });
+
+  cameraSelect.addEventListener('change', async (event) => {
+    const deviceId = event.target.value;
+    await updateCamera(session, deviceId);
+  });
+
+  // Set the initial camera
+  if (cameras.length > 0) {
+    await updateCamera(session, cameras[0].deviceId);
+  }
+}
+
+async function attachLensesToSelect(lenses, session) {
+  lensSelect.innerHTML = ''; // Clear previous options
+  lenses.forEach((lens) => {
+    const option = document.createElement('option');
+    option.value = lens.id;
+    option.text = lens.name;
+    lensSelect.appendChild(option);
+  });
+
+  lensSelect.addEventListener('change', async (event) => {
+    const lensId = event.target.value;
+    const lens = lenses.find((lens) => lens.id === lensId);
+    if (lens) await session.applyLens(lens);
+  });
+}
+
